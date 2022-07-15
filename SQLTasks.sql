04ab157fb53a0d6fd6bfb61043735107c25c210c
Jared Barnes <97368604+JaredBarnes6@users.noreply.github.com>
2022-07-15 10:26:07 -0600

    Update SQLTasks.sql

--- SQLTasks.sql
+++ SQLTasks.sql
@@ -1,39 +1,6 @@
 /* Welcome to the SQL mini project. You will carry out this project partly in
 the PHPMyAdmin interface, and partly in Jupyter via a Python connection.
 
-This is Tier 1 of the case study, which means that there'll be more guidance for you about how to 
-setup your local SQLite connection in PART 2 of the case study. 
-
-The questions in the case study are exactly the same as with Tier 2. 
-
-PART 1: PHPMyAdmin
-You will complete questions 1-9 below in the PHPMyAdmin interface. 
-Log in by pasting the following URL into your browser, and
-using the following Username and Password:
-
-URL: https://sql.springboard.com/
-Username: student
-Password: learn_sql@springboard
-
-The data you need is in the "country_club" database. This database
-contains 3 tables:
-    i) the "Bookings" table,
-    ii) the "Facilities" table, and
-    iii) the "Members" table.
-
-In this case study, you'll be asked a series of questions. You can
-solve them using the platform, but for the final deliverable,
-paste the code for each solution into this script, and upload it
-to your GitHub.
-
-/* Welcome to the SQL mini project. You will carry out this project partly in
-the PHPMyAdmin interface, and partly in Jupyter via a Python connection.
-
-This is Tier 1 of the case study, which means that there'll be more guidance for you about how to 
-setup your local SQLite connection in PART 2 of the case study. 
-
-The questions in the case study are exactly the same as with Tier 2. 
-
 PART 1: PHPMyAdmin
 You will complete questions 1-9 below in the PHPMyAdmin interface. 
 Log in by pasting the following URL into your browser, and
